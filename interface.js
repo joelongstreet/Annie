0dfadfd71659103adaa38f34f1c041c418f7b847
joelongstreet <joelongstreet@gmail.com>
2013-08-23 18:40:22 -0500

    building fun cusomt messaging and adding buttons to the dom

--- interface.js
+++ interface.js
@@ -26,10 +26,25 @@ $(function(){
         }
     })();
 
+
+    // Listen for change functions, save to local storage,
+    // and build the search array
     $('input:checkbox').change(function(){
         var inputName = $(this).attr('name');
         if($(this).is(':checked')){
             localStorage.setItem(inputName, true);
         } else localStorage.setItem(inputName, false);
     });
+
+
+    // Builds a little custom greeting just for fun
+    (function(){
+        var hours   = new Date().getHours();
+        var greeting = '';
+        if(hours < 11) greeting = 'Good Morning'
+        else if(hours < 16) greeting = 'Good Afternoon'
+        else greeting = 'Good Evening'
+
+        $('#greeting').text(greeting);
+    })();
 });
