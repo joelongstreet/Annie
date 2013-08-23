d140287821565b9cb1f1d136a3cd9af0d82b9dcb
joelongstreet <joelongstreet@gmail.com>
2013-08-23 16:57:17 -0500

    first iteration of the mapper, there are probably so many spelling errors in this

---
+++ mapper.js
@@ -0,0 +1,147 @@
+var mapper = {
+    'uid' : {
+        label   : 'Unique Identifier',
+        help    : 'Unique id in the database'
+    },
+    'name' : {
+        label   : 'Name',
+        help    : 'Unique username'
+    },
+    'mail' : {
+        label   : 'E-mail Address'
+    },
+    'created' : {
+        label   : 'Date Created',
+        help    : 'The date the profile was created'
+    },
+    'access' : {
+        label   : 'Last Access Date',
+        help    : 'The date the user last accessed Passport'
+    },
+    'login' : {
+        label   : 'Login Date',
+        help    : 'The date the user last logged in'
+    },
+    'status' : {
+        label   : 'Status',
+        help    : '1 means the user is active, 0 means the user is blocked'
+    },
+    'timezone' : {
+        label   : 'Timezone'
+    },
+    'roles' : {
+        label   : 'Roles'
+    },
+    'og_user_node' : {
+        label   : 'User Node',
+        help    : 'Groups in which the user has subscribed'
+    },
+    'field_bio' : {
+        label   : 'Bio'
+    },
+    'field_first_name' : {
+        label   : 'First Name'
+    },
+    'field_last_name' : {
+        label   : 'Last Name'
+    },
+    'field_user_photo' : {
+        label   : 'Photo URL',
+        help    : 'A web address to the users photo'
+    },
+    'field_notifications' : {
+        label   : 'Notifications',
+        help    : 'What notifications does the user have set'
+    },
+    'field_birthday' : {
+        label   : 'Birthdate'
+    },
+    'field_location' : {
+        label   : 'Location',
+        help    : 'The employees primary office location'
+    },
+    'field_department' : {
+        label   : 'Department'
+    },
+    'field_anniversary' : {
+        label   : 'Anniversary',
+        help    : 'Date the user started as an employee with VML'
+    },
+    'field_work_phone' : {
+        label   : 'Work Phone #'
+    },
+    'field_mobile_phone' : {
+        label   : 'Mobile Phone #'
+    },
+    'field_alternate_phone' : {
+        label   : 'Alternate Phone #'
+    },
+    'field_fax' : {
+        label   : 'Fax #'
+    },
+    'field_preferred_name' : {
+        label   : 'Preferred Name'
+    },
+    'field_home_street' : {
+        label   : 'Street Address',
+        help    : 'A user\'s personal street address' 
+    },
+    'field_home_city' : {
+        label   : 'City',
+        help    : 'A user\'s home city'
+    },
+    'field_home_state' : {
+        label   : 'State',
+        help    : 'A user\'s home state'
+    },
+    'field_home_zip' : {
+        label   : 'Zip Code',
+        help    : 'A user\'s home zip code'
+    },
+    'field_notify_by_email' : {
+        label   : 'Notify by Email?',
+        help    : 'Should notifications be sent by e-mail?'
+    },
+    'field_job_title' : {
+        label   : 'Job Title'
+    },
+    'field_bio_doc' : {
+        label   : 'Official Bio Doc',
+        help    : 'A url pointing to a Microsoft Word document containing the user\'s official VML bio'
+    },
+    'field_bio_official' : {
+        label   : 'Official Bio',
+        help    : 'A text version of a user\'s official VML bio'
+    },
+    'field_im_handle' : {
+        label   : 'Instant Messenger Handle'
+    },
+    'field_personal_email_address' : {
+        label   : 'Personal E-mail Address'
+    },
+    'field_spouse' : {
+        label   : 'Spouse/Partner'
+    },
+    'field_start_date' : {
+        label   : 'Start Date'
+    },
+    'field_twitter_username' : {
+        label   : 'Twitter Username'
+    },
+    'field_updated' : {
+        label   : 'Last Date Updated',
+        help    : 'Date the user\'s profile was last updated'
+    },
+    'cas_name' : {
+        label   : 'CAS Name',
+        help    : 'The user\'s login username'
+    },
+    'display_name' : {
+        label   : 'Display Name',
+        help    : 'A name various interfaces use to display the user\'s name'
+    },
+    'timezone_offset' : {
+        label   : 'Timezone Offset',
+        help    : 'The timezone offset in hours base on the user\'s set timezone'
+    }
+}
